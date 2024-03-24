1fe6ade950fd75b8ba5550c3ca4cc719e3d3697d
hendisantika <hendisantika@yahoo.co.id>
2024-03-24 08:08:48 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootappdocker/SpringBootAppDockerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootappdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppDockerApplication.class, args);
+    }
+
+}
